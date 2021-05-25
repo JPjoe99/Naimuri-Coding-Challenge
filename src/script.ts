97b4b9205cb367090e9483d6299f2d23a3ffbf5f
Joseph Price <joe_price195@outlook.com>
2021-05-25 12:58:10 +0100

    added README fetch

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,15 @@
 let mainElement: HTMLElement = document.querySelector("#main");
-
-let inputElement: HTMLElement = document.querySelector("#input");
-
-let dropdownElement: HTMLElement = document.querySelector("#dropdown-button");
-
 let submitElement: HTMLElement = document.querySelector("#submit");
+
+//Collecting available language filters and adding functions so that
+//the language that's been clicked becomes the language to filter by
 
 let languages: HTMLCollection = document.querySelector(".dropdown-content").children;
 for (let i: number = 0; i < languages.length; i++) {
     languages[i].addEventListener("click", setAsCurrentLanguage);
 }
+
+//Function that manages the change of the language filter
 
 function setAsCurrentLanguage(e: Event): void {
     let currentLang = document.querySelector(".lang.active");
@@ -19,6 +19,8 @@ function setAsCurrentLanguage(e: Event): void {
 }
 
 submitElement.addEventListener("click", sendRequestToGitHubAPI);
+
+//Function to find the user's chosen filters and return them as a JSON object
 
 function obtainFilters(): JSON {
     let userFilter: HTMLInputElement = document.querySelector("#user");
@@ -32,30 +34,39 @@ function obtainFilters(): JSON {
     return <JSON>filterJSON;
 }
 
-function clearCurrentRepos(): void {
+function clearCurrentRepositories(): void {
     mainElement.innerHTML = "";
 }
 
+//Function to constructor the API call based on the user's selected filters
+
 function constructGitHubAPIRequest(): string {
     let filterJSON: any = obtainFilters();
-    console.log(filterJSON);
     let GitHubAPICall: string = `https://api.github.com/search/repositories?q=`;
     let repo = filterJSON.repo;
     GitHubAPICall += `${repo}`;
+    if (filterJSON.language != ``) {
+        if (filterJSON.language == "All" && filterJSON.user == "") {
+            GitHubAPICall += `+language:`;
+        }
+        else {
+            GitHubAPICall +=  `+language:${filterJSON.language}`;
+        }
+    }
     if (filterJSON.user != ``) {
         GitHubAPICall += `+user:${filterJSON.user}`;
     }
-    if (filterJSON.language != ``) {
-        GitHubAPICall += `+language:${filterJSON.language}`;
-    }
+  
     return GitHubAPICall;
 }
 
+//Function to send request to GitHub API using selected filters by the user
+
 function sendRequestToGitHubAPI(e: Event): void {
-    clearCurrentRepos();
+    //Clear the current repositories on the screen and create the new API call
+    clearCurrentRepositories();
     constructGitHubAPIRequest();
     let GitHubAPICall: string = constructGitHubAPIRequest();
-    console.log(GitHubAPICall);
     fetch(GitHubAPICall, {
         method: "GET",
         headers: {
@@ -66,81 +77,106 @@ function sendRequestToGitHubAPI(e: Event): void {
         return res.json();
     })
     .then(data => {
+        //Checks to see if any repositories match the user's filters
         if (data.items.length == 0) {
             mainElement.innerHTML = `<h1 class="text-center">No repositories found!</h1>`;
         }
-        console.log(data.items);
-        outputToDocument(data.items);
+        //return data;
+        outputRepositoriesToDocument(data.items);
     })
     .catch(error => {
         console.log(error);
     })
 }
 
-function getReadMe() {
+//Function to fetch the README of a given repository
 
-}
-
-function fetchReadMe(item: any): any {
-    fetch(`https://api.github.com/repos/${item.owner.login}/${item.name}/readme`)
+function fetchReadMe(item: any): void {
+    fetch(`https://api.github.com/repos/${item.owner.login}/${item.name}/readme`, {
+        method: "GET",
+        headers: {
+            //Accept is set like this to return the README as rendered HTML
+            "Accept": "application/vnd.github.html"
+        }
+    })
     .then(res => {
-        return res.json();
+        //Check to see if the API rate limit has been exceeded
+        if (res.status == 403) {
+            return;
+        }
+        else {
+            return res.text();
+        }
     })
-    .then(data => {
-        //let readMe = atob(data.content);
-        //console.log(readMe);
-        console.log(data);
-        return data.content;
+    .then(html => {
+        //Append the README to the main body of the document
+        let readMeElement: HTMLDivElement = document.createElement("div");
+        readMeElement.className = "text-center";
+        if (html == undefined) {
+            readMeElement.innerHTML = `<h2>No ReadMe found</h2>
+                                       <h4>API rate limit has been exceeded<h4>
+                                      `;
+        }
+        else {
+            readMeElement.innerHTML = html;
+        }
+        let repo = document.querySelector(`#${item.name}`);
+        repo.appendChild(readMeElement);
     })
     .catch(error => {
         console.log(error);
     })
 }
 
-function constructRepoCardHTML(item: any): HTMLElement {
+//Function to construct the HTML to display the data of each repository returned from
+//the search
+
+function constructRepositoryHTML(item: any): HTMLElement {
     let repoElement: HTMLElement = document.createElement("div");
-    //fetchReadMe(item);
     repoElement.id = `${item.id}`;
-    repoElement.className = "col-6 repo-card";
+    repoElement.className = "col-12 repo-card";
     let repoInnerHTML: string = ``;
     repoInnerHTML += `<h2 class="text-center"><a id="repo-header" href="${item.html_url}" target="_blank">${item.name}</a></h2>
                       <h3 class="text-center">By <a id="repo-header" href="${item.owner.html_url}" target="_blank">${item.owner.login}</a></h3>
-                      <p class="text-center">${item.description}</p>
                       <hr>
-                      <h3 class="text-center">Key Details</h3>
                       <div class="row">
-                        <div class="col-4">
-                          <h4 class="text-center">Forks</h4>
-                          <h1 class="text-center">${item.forks_count}</h1>
-                        </div>
-                        <div class="col-4">
-                          <h4 class="text-center">Stars</h4>
-                          <h1 class="text-center">${item.stargazers_count}</h1>
-                        </div>
-                        <div class="col-4">
-                          <h4 class="text-center">Issues</h4>
-                          <h1 class="text-center">${item.open_issues_count}</h1>
-                        </div>
+                          <div class="col-4">
+                              <h3 class="text-center">Key Details</h3>
+                              <div class="row">
+                                  <div class="col-4">
+                                      <h4 class="text-center">Forks</h4>
+                                      <h1 class="text-center">${item.forks_count}</h1>
+                                  </div>
+                                  <div class="col-4">
+                                      <h4 class="text-center">Stars</h4>
+                                      <h1 class="text-center">${item.stargazers_count}</h1>
+                                  </div>
+                                  <div class="col-4">
+                                      <h4 class="text-center">Issues</h4>
+                                      <h1 class="text-center">${item.open_issues_count}</h1>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-8">
+                              <div id =${item.name}></div>
+                          </div>
                       </div>
-
-                      <p>Read Me:</p>
                       `;
     repoElement.innerHTML = repoInnerHTML;
+    //Fetch the README of the repository and append to the above HTML
+    fetchReadMe(item)
     return repoElement;
 }
 
+//Function that iterates through each repository that's been returned and construct
+//its HTML using the constructRepoCardHTML function and appends to the main document
 
-
-
-function outputToDocument(items: Array<any>): void {
+function outputRepositoriesToDocument(items: Array<any>): void {
     let repoElement: HTMLElement =  document.createElement("div");
     repoElement.className = "row";
     let constructedRepoElement: HTMLElement;
     for (let i: number = 0; i < items.length; i++) {
-        constructedRepoElement = constructRepoCardHTML(items[i]);
-        //constructedRepoElement = fetchRepositoryCardSnippet();
-        //insertDataIntoCard(constructedRepoElement, items[i]);
-        //console.log("repo element" + constructedRepoElement);
+        constructedRepoElement = constructRepositoryHTML(items[i]);
         repoElement.appendChild(constructedRepoElement);
     }
     mainElement.appendChild(repoElement);
