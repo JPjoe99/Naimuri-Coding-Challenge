aefb8aae91c71393fdddfca5601bcb5b572d99c7
Joseph Price <joe_price195@outlook.com>
2021-05-29 16:28:58 +0100

    Added outputNoRepositoriesFound()

--- src/Output.ts
+++ src/Output.ts
@@ -3,7 +3,7 @@ import { Repository } from "./Repository";
 class Output {
     private mainBody: HTMLElement;
     constructor() {
-        this.mainBody = document.querySelector("#main");
+        this.mainBody = document.querySelector("#main"); 
     }
     getMainBody(): HTMLElement {
         return this.mainBody;
@@ -11,16 +11,7 @@ class Output {
     clearMainBody(): void {
         this.mainBody.innerHTML = "";
     }
-    drawRepositoryCards(repositories: Array<Repository>) {
-        this.clearMainBody();
-        let repositoryCardsHTML: HTMLDivElement = document.createElement("div");
-        repositoryCardsHTML.className = "row";
-        for (let i: number = 0; i < repositories.length; i++) {
-            repositoryCardsHTML.appendChild(this.drawRepositoryCard(repositories[i]));
-        }
-        this.getMainBody().appendChild(repositoryCardsHTML);
-    }
-    drawRepositoryCard(repository: Repository): HTMLDivElement {
+    drawRepositoryCard(repository: Repository): void {
         let repoElement: HTMLDivElement = document.createElement("div");
         repoElement.className = "col-12 repo-card";
         let repoInnerHTML: string = ``;
@@ -28,7 +19,7 @@ class Output {
                         <h3 class="text-center">By <a id="repo-header" href="${repository.getAuthorURL()}" target="_blank">${repository.getAuthor()}</a></h3>
                         <hr>
                         <div class="row">
-                            <div class="col-4">
+                            <div class="col-4 col-s-12">
                                 <h3 class="text-center">Key Details</h3>
                                 <div class="row">
                                     <div class="col-4">
@@ -45,13 +36,18 @@ class Output {
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-8">
-                                <div id =${repository.getName()}></div>
+                            <div class="col-8 col-s-12">
+                                <div id="README">${repository.getREADME()}</div>
                             </div>
                         </div>
                         `;
         repoElement.innerHTML = repoInnerHTML;
-        return repoElement;
+        this.getMainBody().appendChild(repoElement);
+    }
+    outputNoRepositoriesFound(): void {
+        let noReposFoundElement: HTMLDivElement = document.createElement("div");
+        noReposFoundElement.innerHTML = `<h2 class="text-center">No repositories found under these filters</h2>`;
+        this.getMainBody().appendChild(noReposFoundElement);
     }
 }
 
